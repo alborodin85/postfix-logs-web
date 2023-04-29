3a1966f4d9af73b1fe0fad8dd9b4d77c7648b535
Alexandr Borodin <alborodin85@mail.ru>
2023-04-29 19:49:56 +0300

    Переведен vite на mix

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+let mix = require('laravel-mix');
+let fs = require('node:fs');
+
+const isProd = mix.inProduction();
+
+mix.copyDirectory('resources/images', 'public/images');
+fs.rmSync('./public/storage', {force: true});
+fs.symlinkSync('../storage/app/public', './public/storage', "dir");
+mix.ts('resources/js/app.ts', 'public/js/app.js').vue({version: 3});
+mix.sass('resources/scss/styles.scss', 'public/css/styles.css');
+
+mix.sourceMaps(false, 'source-map');
+
+const watchType = process.env.MIX_WATCH_TYPE
+if (watchType === 'browsersync') {
+    mix.browserSync({port: 80, open: false, server: 'public/' + displayVersion, 'files': 'public/' + displayVersion});
+}
