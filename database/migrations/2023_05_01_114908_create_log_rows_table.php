49e9c9b01e6fbca5005cdad7c1fc43e493124515
Alexandr Borodin <alborodin85@mail.ru>
2023-05-12 21:28:35 +0300

    Добавлена модель и миграция для одной записи текущего лога

---
+++ database/migrations/2023_05_01_114908_create_log_rows_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_log_rows', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('dateTime');
+            $table->string('hostName');
+            $table->string('module');
+            $table->integer('procId');
+            $table->string('queueId')->nullable();
+            $table->string('errorLevel')->nullable();
+            $table->text('rowText')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_log_rows');
+    }
+};
