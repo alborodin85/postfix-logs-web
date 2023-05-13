b54b91457a9f3392594c096aa5ec7aeaf063a256
Alexandr Borodin <alborodin85@mail.ru>
2023-05-13 22:47:06 +0300

    Сделан объект ApiResponse

---
+++ app/Services/ApiResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Response;
+
+class ApiResponse
+{
+    public string $payload;
+    public int $errorCode;
+    public string $errorModule;
+    public string $errorText;
+
+    public function __construct(string $jsonResponse)
+    {
+        $arResponse = json_decode($jsonResponse, true);
+
+        $errorMessage = 'получен некорректный json-response';
+        if (!is_array($arResponse)) {
+            abort(500, $errorMessage);
+        }
+
+        $responseFields = ['payload', 'errorCode', 'errorModule', 'errorText'];
+
+        if (array_keys($arResponse) != $responseFields) {
+            abort(500, $errorMessage);
+        }
+
+        $this->payload = $arResponse['payload'];
+        $this->errorCode = $arResponse['errorCode'];
+        $this->errorModule = $arResponse['errorModule'];
+        $this->errorText = $arResponse['errorText'];
+    }
+
+    public static function buildApiResponse(
+        string $payload, int $errorCode = 0, string $errorModule = '', string $errorText = ''
+    ): Response {
+
+        $content = [
+            'payload' => $payload,
+            'errorCode' => $errorCode,
+            'errorModule' => $errorModule,
+            'errorText' => $errorText,
+        ];
+
+        $encodedContent = json_encode($content, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+
+        return \response($encodedContent)->header('content-type', 'application/json');
+    }
+}
