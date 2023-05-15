8a0878e43e0748728d2dd60a64d44529f1785c57
Alexandr Borodin <alborodin85@mail.ru>
2023-05-15 23:41:25 +0300

    Правки для возможности работы по https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,5 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        if (config('app.force_https')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'force_https' => env('HTTP_SCHEMA') == 'https',
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
